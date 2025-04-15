00406c27a74319899441a930403b0375910d5e28
Yurii Ruban <yurii.ruban97@gmail.com>
2025-04-15 15:05:30 +0100

    feat: Implement today forecast and conditions

--- src/js/config.js
+++ src/js/config.js
@@ -4,29 +4,56 @@ const TEMP = {
 };
 
 const WINDSPEED = {
-    km: "km/h",
-    m: "m/s"
+    kmh: "km/h",
+    ms: "m/s"
 };
 
 const HUMIDITY = {
     percent: '%'
 };
 
+const TIMEFORMAT = {
+    h12: 12,
+    h24: 24
+};
+
 const CONFIG = {
     temp: TEMP.celsius,
-    wind: WINDSPEED.m,
+    wind: WINDSPEED.ms,
     humidity: HUMIDITY.percent,
     forecastMaxDays: 7,
     imageFolder: '/img/weather',
     imageFormat: 'svg',
+    timeFormat: TIMEFORMAT.h12,
 
     convertTemperature: function (temp) {
         const currentFar = parseFloat(temp);
-        return temp === TEMP.farenheit ? currentFar : ((currentFar - 32) / 1.8).toFixed(1);
+        return this.temp === TEMP.farenheit ? currentFar : ((currentFar - 32) / 1.8).toFixed(1);
+    },
+
+    convertSpeed: function (speed) {
+        const currentSpeed = parseFloat(speed);
+        return this.wind === WINDSPEED.kmh ? (currentSpeed * 3.6).toFixed(1) : currentSpeed;
     },
 
     getImagePath: function (iconName) {
         return `${this.imageFolder}/${iconName}.${this.imageFormat}`;
+    },
+
+    getTimeString: function (time) {
+        let timestr = '';
+        if (this.timeFormat === TIMEFORMAT.h24) {
+            timestr = `${time}:00`;
+        } else {
+            if (time < TIMEFORMAT.h12) {
+                timestr = `${time}:00 AM`;
+            } else if (time > TIMEFORMAT.h12) {
+                timestr = `${time - TIMEFORMAT.h12}:00 PM`;
+            } else {
+                timestr = `${time}:00 PM`;
+            }
+        }
+        return timestr;
     }
 
 };
--- src/js/view.js
+++ src/js/view.js
@@ -13,14 +13,39 @@ const fillMainSection = function (data) {
     conditionsText.innerText = data.main.conditions || errorStr;
     tempText.innerText = globalConfig.convertTemperature(data.main.temp) + globalConfig.temp || errorStr;
     weatherImage.src = globalConfig.getImagePath(data.main.iconText);
+}
 
+const fillTodaysForecast = function (data) {
+    const forecastContainer = document.querySelector('.forecast-container');
+    Array.from(forecastContainer.children).forEach((forecastItem, index) => {
+        const timeTextItem = forecastItem.querySelector('.time-text');
+        const iconItem = forecastItem.querySelector('img');
+        const tempItem = forecastItem.querySelector('.temp');
+
+        timeTextItem.innerText = globalConfig.getTimeString(data.forecast[index].time);
+        iconItem.src = globalConfig.getImagePath(data.forecast[index].iconText);
+        tempItem.innerText = globalConfig.convertTemperature(data.forecast[index].temp) + globalConfig.temp || errorStr;
+    });
+}
+
+const fillAirConditions = function (data) {
+    const conditionsContainer = document.querySelector('.conditions-container');
+    const realFeel = conditionsContainer.querySelector('.real-feel');
+    const hummidity = conditionsContainer.querySelector('.hummidity');
+    const wind = conditionsContainer.querySelector('.wind');
+    const uvIndex = conditionsContainer.querySelector('.uv-index');
+
+    realFeel.innerText = globalConfig.convertTemperature(data.airConditions.feelslike) + globalConfig.temp || errorStr;
+    hummidity.innerText = data.airConditions.humidity + globalConfig.humidity;
+    wind.innerText = `${globalConfig.convertSpeed(data.airConditions.windspeed)} ${globalConfig.wind}`;
+    uvIndex.innerText = data.airConditions.uvindex;
 }
 
 const filler = {
     fillInfo: function (data) {
         fillMainSection(data);
-        // fillTodaysForecast(data);
-        // fillAirConditions(data);
+        fillTodaysForecast(data);
+        fillAirConditions(data);
         // fillWeekForecast(data);
     }
 };
